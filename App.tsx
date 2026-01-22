7c1737671e6b077bc476336a8c664baff05257f5
Harshal Vibhandik <harshal.vibhandik@capgemini.com>
2026-01-22 21:13:35 +0530

    Learned about the Platoform to set dynamic text and color for Android & iOS different platforms

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is {Platform.OS === "android" ? "Android" : "iOS"} Device</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Platform.OS === "android" ? "#f0f8ff" : "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
