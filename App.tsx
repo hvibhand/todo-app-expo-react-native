a07bbca1d87c34d0c64a0aa2c294ef63ba06d3eb
Harshal Vibhandik <harshal.vibhandik@capgemini.com>
2026-01-23 15:39:07 +0530

    Added code to show Increase and Descrease counter small App using useState from react

--- App.tsx
+++ App.tsx
@@ -1,63 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
-  const handlePressablePress = () => {
-    alert('Pressable Pressed!');
+  var [counter, setCounter] = useState(0);
+  const handleCounterIncrease = () => {
+    // Increase counter logic here
+    setCounter(counter + 1);
+  }
+  const handleCounterDecrease = () => {
+    // Decrease counter logic here
+    setCounter(counter - 1);
   }
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <StatusBar style="auto" />
-      <ScrollView 
-        showsVerticalScrollIndicator={false}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{alignItems: 'center'}}
-      >
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-        <Image
-          source={require('./assets/icon.png')}
-          style={styles.image1}
-        />
-      </ScrollView>
-    </SafeAreaView>
+      <Button title="Increase" onPress={handleCounterIncrease} />
+      <Text style={{ fontSize: 50, padding: 20 }}>{counter}</Text>
+      <Button title="Decrease" onPress={handleCounterDecrease} />
+    </View>
   );
 }
 
@@ -65,18 +28,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  image1: {
-    width: 200,
-    height: 200,
-    alignSelf: 'center',
-    margin: 20
-  },
-  image2: {
-    width: 200,
-    height: 200,
-    alignSelf: 'center',
-    marginTop: 20,
-    borderRadius: 100
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
